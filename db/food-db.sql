4a61eee3faf4912c84a9e2e26c050d5186d15f26
Jamesbarford <jamesbarfordevans@gmail.com>
2020-06-18 08:16:32 +0100

    code formatting

--- db/food-db.sql
+++ db/food-db.sql
@@ -11,12 +11,13 @@ CREATE TABLE IF NOT EXISTS "public"."backbone" (
 \copy backbone(ingredient_1, ingredient_2, ingredient_int, category, prevalence) FROM '../data/backbone.csv' DELIMITER ',' CSV HEADER;
 
 
-CREATE TABLE IF NOT EXISTS  "public"."comp_info" (
+CREATE TABLE IF NOT EXISTS "public"."comp_info" (
     "id" int4 NOT NULL,
     "compound_name" bpchar(100) NOT NULL,
     "cas_number" bpchar(100) NOT NULL
 );
 \copy comp_info(id, compound_name, cas_number) FROM '../data/comp_info.tsv' DELIMITER E'\t' CSV HEADER;
+
 
 CREATE TABLE IF NOT EXISTS  "public"."ingr_comp" (
     "ingredient_id" int4 NOT NULL,
@@ -24,7 +25,8 @@ CREATE TABLE IF NOT EXISTS  "public"."ingr_comp" (
 );
 \copy ingr_comp(ingredient_id, compound_id) FROM '../data/ingr_comp.tsv' DELIMITER E'\t' CSV HEADER;
 
-CREATE TABLE IF NOT EXISTS  "public"."ingr_info_converted" (
+
+CREATE TABLE IF NOT EXISTS "public"."ingr_info_converted" (
     "id" int4 NOT NULL,
     "ingredient_name" bpchar(100) NOT NULL,
     "category" text NOT NULL
@@ -32,7 +34,7 @@ CREATE TABLE IF NOT EXISTS  "public"."ingr_info_converted" (
 \copy ingr_info_converted(id, ingredient_name, category) FROM '../data/ingr_info.tsv' DELIMITER E'\t' CSV HEADER;
 
 
-CREATE TABLE IF NOT EXISTS  "public"."srep-s2" (
+CREATE TABLE IF NOT EXISTS "public"."srep-s2" (
     "ingredient_1" bpchar(100) NOT NULL,
     "ingredient_2" bpchar(100) NOT NULL,
     "shared" int4 NOT NULL,
@@ -40,17 +42,16 @@ CREATE TABLE IF NOT EXISTS  "public"."srep-s2" (
 );
 \copy "srep-s2"(ingredient_1, ingredient_2, shared) FROM '../data/srep00196-s2.csv' DELIMITER ',' CSV HEADER;
 
-CREATE TEMPORARY TABLE temp_table(
+
+CREATE TEMPORARY TABLE "temp_table" (
     "all_values" text
  );
-
 \copy "temp_table"("all_values") FROM '../data/srep00196-s3.csv' DELIMITER ';' CSV;
 
-
 CREATE TABLE "public"."srep-s3" AS
-WITH T1 AS (
-    SELECT
-        split_part(all_values, ',', 1) as cusine,
-        string_to_array(all_values, ',') as igredients
-    FROM temp_table
-) SELECT cusine, igredients[2:] FROM T1;
+    WITH T1 AS (
+        SELECT
+            split_part(all_values, ',', 1) as cusine,
+            string_to_array(all_values, ',') as igredients
+        FROM temp_table
+    ) SELECT cusine, igredients[2:] FROM T1;
